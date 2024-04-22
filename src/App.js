54d735b81bc68b666b19692a8de2c5ecac5138d8
Harkeerat <34791715+eKeiran@users.noreply.github.com>
2024-04-23 02:46:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import {
   Route
 } from "react-router-dom";
 
-import Navbar from './components/Navbar/Navbar';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import SkillsPage from './pages/Skills';
-import Contact from './pages/Contact';
-import Footer from './components/Footer/Footer';
+
 import './App.css';
 
 const App = () => {
@@ -18,19 +13,12 @@ const App = () => {
   return (
     <BrowserRouter>
       <div id='top' className={`dark app`}>
-        <Navbar />
-
+        
         <main>
-          <Routes>
-            <Route path='/' element={<About />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/skills' element={<SkillsPage />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
+     
         </main>
         
-        <Footer />
-      </div>
+     </div>
     </BrowserRouter>
   );
 }
